4f9e821e623021cde59efb225cf55860d32a0acc
SlyCedix <SlyCedix@gmail.com>
2018-11-11 19:47:57 -0500

    clean up and add streamer tab completion

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 function start() {
     document.getElementById('keywords').focus();
-    l = getParameterByName('l');
-    $('.main li').css( 'color', l);
 
     window.setInterval(function() {
         updatetime();
@@ -10,13 +8,12 @@ function start() {
 
 function handleKeyPress(e) {
     var key = e.keyCode || e.which;
-    if (key == 13) {
-        var text = document.getElementById("keywords").value;
-        var e = getParameterByName('s');
+    var text = document.getElementById("keywords").value;
+    var option = text.substr(1,text.indexOf(' ')-1);
+    var subtext = text.substr(2 + option.length);
+    if (key == 13) {                                                        // Search functions
         if (text[0] === ';'){
             if(text.indexOf(' ') > -1){
-                var option = text.substr(1,text.indexOf(' ')-1);
-                var subtext = text.substr(2 + option.length);
                 switch(option){
                     case "g":
                         window.location = "https://www.google.com/search?q=" + subtext;
@@ -55,18 +52,34 @@ function handleKeyPress(e) {
                         break;
                 }
             }
-        } else if (e == 'g') {
-            window.location = "https://www.google.com/search?q=" + text;
         } else {
             window.location = "https://duckduckgo.com/?q=" + text;
         }
     }
+    if (key == 9) {                                                         // Tab Completion Functions
+        e.preventDefault();
+        e.stopPropagation();
+        if(text[0] === ';'){
+            switch(option){
+                case 't':
+                    var streamers = ['moonmoon_ow', 'admiralbahroo', 'witwix'];
+                    for(var streamer of streamers){
+                        if(subtext && subtext === streamer.substr(0, subtext.length)){
+                            document.getElementById("keywords").value = ';t ' + streamer;
+                            break;
+                        }
+                    }
+                    break;
+            }
+        }
+    }
 }
 
-document.body.onkeyup = function(e) {
-    if (e.keyCode == 32) {
-    	document.getElementById("keywords").focus();
-    }
+document.body.onkeyup = function(e) {                                       // Space to go to search bar
+    var text = document.getElementById("keywords").value;
+    var option = text.substr(0,text.indexOf(' ')-1);
+    if (e.keyCode == 32) document.getElementById("keywords").focus(); 
+
 }
 
 function updatetime() {
@@ -81,14 +94,4 @@ function updatetime() {
     }
     var time = hours + ' ' + mins;
     document.getElementById("time").innerHTML = time;
-}
-
-function getParameterByName(name, url) {
-    if (!url) url = window.location.href;
-    name = name.replace(/[\[\]]/g, "\\$&");
-    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
